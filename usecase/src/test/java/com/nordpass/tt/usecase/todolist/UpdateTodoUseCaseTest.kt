8dd85a33fc35f857b1dce87b1dedaca34ca9d8b8
istvanmunkacsi <istvan.munkacsi@revelane.com>
2021-10-31 19:09:07 +0100

    Bonus Task 3: - Added test for UpdateTodoUseCaseTest

---
+++ usecase/src/test/java/com/nordpass/tt/usecase/todolist/UpdateTodoUseCaseTest.kt
@@ -0,0 +1,32 @@
+package com.nordpass.tt.usecase.todolist
+
+import com.nhaarman.mockitokotlin2.given
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nordpass.tt.usecase.data.Todo
+import io.reactivex.Completable
+import org.junit.Before
+import org.junit.Test
+
+class UpdateTodoUseCaseTest {
+    private lateinit var useCase: UpdateTodoUseCase
+    private val api: TodoListApi = mock()
+    private val storage: TodoStorage = mock()
+
+    @Before
+    fun setup() {
+        useCase = UpdateTodoUseCase(storage)
+    }
+
+    @Test
+    fun update_itemUpdated() {
+        val todo: Todo = mock()
+        given(storage.update(todo)).willReturn(Completable.complete())
+
+        useCase.update(todo)
+            .test()
+            .assertOf { verify(storage).update(todo) }
+            .assertComplete()
+            .assertNoErrors()
+    }
+}
